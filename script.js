9ee113b9c7c198c9853fc09359262f13e5afe208
Benaiah Enebeli <258831278+benaiah324@users.noreply.github.com>
2026-02-11 10:02:01 +0100

    Added playGame function
    The index.html was corrected for misspelling
    playGame function was added to the script.js  and the game can be played for 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// function getComputerChoice(params) {
-//     let randomNum = Math.floor(Math.random()*3) + 1
-//     let choice = randomNum == 1 ? "rock": randomNum == 2 ? "paper" : randomNum == 3 ? "scissors" : "Invalid Option!"
-//     return choice
-// }
 
 // Get Computer choice
 function getComputerChoice() {
@@ -23,58 +18,67 @@ function getHumanChoice() {
   const choiceIndex = Number(input);
 
   if (Number.isNaN(choiceIndex) || choiceIndex < 0 || choiceIndex >= CHOICES.length) {
-    return null; // caller decides how to handle invalid input
+    alert(`Invalid selction: ${choiceIndex}`)
+    return ; // caller decides how to handle invalid input
   }
 
   return CHOICES[choiceIndex];
 }
 
-let humanScore = 0, computerScore = 0 
+// Playing the game
+function playGame() {
+    let rounds = 1
+    let humanScore = 0, computerScore = 0 
 
-// function playRound(humanChoice, computerChoice) {
-//     if(humanChoice == computerChoice){
-//         return `Its a Draw! You both choose ${humanChoice}`
-//     }
+    while (rounds <= 5) {
+        const humanSelection = getHumanChoice()
+        const computerSelection  = getComputerChoice()
 
-//     if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerChoice == 'paper') {
-//         return `You Win! ${humanChoice} beats ${computerChoice}`
-//     } else{
-//         return `You Lose! ${computerChoice} beats ${humanChoice}`
-//     }
-    
-// }
+        let roundResult = playRound(humanSelection, computerSelection)
+        console.log(`Round ${rounds}:
+          ${roundResult.result}: ${roundResult.message}`)
+        rounds++
+    }
+    console.log(getWinner(humanScore, computerScore))
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return {
-      result: "draw",
-      message: `It's a draw! You both chose ${humanChoice}.`,
+  // Playing a single round
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return {
+        result: "draw",
+        message: `It's a draw! You both chose ${humanChoice}.`,
+      };
+    }
+
+    const winningRules = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
     };
-  }
 
-  const winningRules = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper",
-  };
+    const humanWins = winningRules[humanChoice] === computerChoice;
 
-  const humanWins = winningRules[humanChoice] === computerChoice;
-
-  return humanWins
-    ? {
+    return humanWins
+      ? {
         result: "win",
         message: `You win! ${humanChoice} beats ${computerChoice}.`,
+        human: humanScore += 1,
       }
-    : {
+      : {
         result: "lose",
         message: `You lose! ${computerChoice} beats ${humanChoice}.`,
+        computer: computerScore += 1
       };
+  }
+  // Getting the winner after the full rounds
+  function getWinner(humanScore, computerScore) {
+    console.log(`Player Score: ${humanScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+
+    let winMessage = humanScore > computerScore ? `Hurray you are the Winner after 5 rounds` : humanScore < computerScore ? `Sorry you lost after 5 rounds` : `Draw!!! No Victor, No Vanquish`
+
+    return winMessage
+  }
 }
 
-
-const humanSelection = getHumanChoice()
-const computerSelection  = getComputerChoice()
-
-const roundResult = playRound(humanSelection, computerSelection)
-
-console.log(roundResult.message)
+playGame()
